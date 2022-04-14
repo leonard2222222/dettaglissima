242f18be725b80b55b4e18b73c56eccdc0a7b0e3
Matteo <cereteo9870@gmail.com>
2022-04-14 09:53:25 +0200

    Fix page

---
+++ js/UtilsJs.js
@@ -0,0 +1,7 @@
+    function toHash(str) {
+        var hash = 5381, i = str.length;
+        while (i) {
+            hash = hash * 33 ^ str.charCodeAt(--i)
+        }
+        return hash >>> 0
+    }
